ef7631655d9f4493d977697bc130b45fa050b18c
MonsterFich <sasha.kulik.21200@gmail.com>
2020-01-15 22:41:10 +0200

    Created class for sorting elements.

--- XMLDocGen/Tree/Container.cs
+++ XMLDocGen/Tree/Container.cs
@@ -13,33 +13,54 @@ namespace XMLDocGen
         public Container (List<XMLDocGen.Models.Member> members)
         {
             Members = members;
+            Nodes = new List<Tree.Models.INode>();
+            processing();
         }
 
-        private bool isExistNode(string name, List<Tree.Models.INode> sNode)
+        private List<Tree.Models.INode> getNeededNode(string[] names, List<Tree.Models.INode> sNode)
         {
-            try {
-                sNode.Find(x => x.Name == name); 
+            for (int i=0;i<names.Length-1;i++)
+            {
+                if (sNode.Find(x => x.Name == names[i]) == null)
+                {
+                    sNode.Add(new Tree.Models.NodeNamespace(names[i]));
+                }
+                sNode = sNode.Find(x => x.Name == names[i]).Child;
+                i++;
             }
-            catch (ArgumentNullException){
-                return false;
-            }
-            return true;
+            return sNode;
         }
 
         private void processing()
         {
             foreach (var member in Members)
             {
-                string[] sArr = member.Name.Split(".");
-                List<Tree.Models.INode> sNode = Nodes;
-                for (int i = 0; i < sArr.Length - 1; i++)
+                if (!member.Name.Contains("System."))
                 {
-                    sNode.Find(x => x.Name == sArr[i]);
-                }
-                switch (member)
-                {
-                    case Models.Field fld:
-                        break;
+                    List<Tree.Models.INode> Node = Nodes;
+                    string[] sArr = member.Name.Split(".");
+                    if (sArr.Length > 1)
+                    {
+                        Node = getNeededNode(sArr, Nodes);
+                    }
+                    switch (member)
+                    {
+                        case Models.Field fld:
+                            Node.Add(new Tree.Models.NodeField(sArr[sArr.Length - 1], fld.Description));
+                            break;
+                        case Models.Method mth:
+                            Node.Add(new Tree.Models.NodeMethod(sArr[sArr.Length - 1], mth.Description, mth.Args, mth.Returns));
+                            break;
+                        case Models.Property prt:
+                            Node.Add(new Tree.Models.NodeProperty(sArr[sArr.Length - 1], prt.Description));
+                            break;
+                        case Models.Type tp:
+                            Node.Add(new Tree.Models.NodeType(sArr[sArr.Length - 1], tp.Description));
+                            break;
+                        default:
+                            Node.Add(new Tree.Models.NodeUndef(sArr[sArr.Length - 1]));
+                            break;
+                    }
                 }
             }
         }
